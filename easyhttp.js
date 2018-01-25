bf1492e1674ebce146464efc217c71b247f8d8c2
akintola olalekan <akinlekan@gmail.com>
2018-01-25 19:27:35 +0100

    Refactored to use async await

--- easyhttp.js
+++ easyhttp.js
@@ -2,7 +2,7 @@
  * EasyHTTP Library
  * Library for making HTTP requests
  * 
- * @version 2.0.0
+ * @version 3.0.0
  * @author Akintola Olalekan
  * @license MIT
  * 
@@ -11,59 +11,50 @@
 
  class EasyHTTP {
    //Make an HTTP GET Request
-   get(url){
-    return new Promise((resolve, reject) => {
-      fetch(url)
-        .then(res => res.json())
-        .then(data => resolve(data))
-        .catch(err => reject(err))
-    });
+   async get(url){
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
    }
 
    //Make an HTTP Post Request
-   post(url, data){
-     return new Promise((resolve, reject) => {
-       fetch(url, {
+   async post(url, data){
+      const response = await fetch(url, {
          method: 'POST',
          headers: {
            'Content-type': 'application/json'
          },
          body: JSON.stringify(data)
-       })
-         .then(res => res.json())
-         .then(data => resolve(data))
-         .catch(err => reject(err))
-     });
+       });
+
+       const resData = await response.json();
+       return resData;
    }
 
    //Make an HTTP Put Request
-   put(url, data){
-     return new Promise((resolve, reject) => {
-       fetch(url, {
+   async put(url, data){
+      const response = await fetch(url, {
          method: 'PUT',
          headers: {
            'Content-type': 'application/json'
          },
          body: JSON.stringify(data)
-       })
-         .then(res => res.json())
-         .then(data => resolve(data))
-         .catch(err => reject(err))
-     });
+       });
+
+      const resData = await response.json();
+      return resData;
    }
 
    //Make an HTTP Delete Request
-   delete(url) {
-     return new Promise((resolve, reject) => {
-       fetch(url, {
+   async delete(url) {
+      const response = await fetch(url, {
          method: 'DELETE',
          headers: {
            'Content-type': 'application/json'
          }
-       })
-         .then(res => res.json())
-         .then(() => resolve('Resource Deleted'))
-         .catch(err => reject(err))
-     });
+       });
+
+     const resData = await 'Resource Deleted';
+     return resData; 
    }
  }
